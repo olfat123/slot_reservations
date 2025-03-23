e299e318df75469dca12e2dffc1bea688f05d48a
OlfatProgressus <olfat@progressus.io>
2025-03-23 12:16:30 +0200

    add style to thank you page

--- templates/thank-you.php
+++ templates/thank-you.php
@@ -8,7 +8,7 @@ if (!defined('ABSPATH')) {
 }
 get_header();
 ?>
- <div class="container">
+ <div class="thank-you-container">
     <div class="check-icon"></div>
     <h1>Thank You!</h1>
     <p>Your reservation has been successfully completed. We'll send you a confirmation email with all the details shortly. If you have any questions, feel free to contact us.</p>
